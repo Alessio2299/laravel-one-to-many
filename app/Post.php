0571349bff67b9c3d86b688c891af3b8c47164c6
Alessio Napoli <alessionapoli99@icloud.com>
2022-04-07 15:30:16 +0200

    Created Model Category and added Migration, Seeder and Foreign Key

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'url', 'description', 'slug'];
+
+    public function categories(){
+        return $this->hasMany('App\Category');
+    }
 }
